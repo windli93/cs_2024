05bd2fbc49ced38c29c2ef71af50f62494cbae26
李红健 <hongjian.li@patpat.com>
2024-09-12 15:45:10 +0800

    提交链表题目

---
+++ leetcode/com.basic/node/IntersectionTwoLinkedLists_160.java
@@ -0,0 +1,37 @@
+package node;
+
+/**
+ * @Author hongjian.li
+ * @Description
+ * @Date 9/12/2024 3:33 PM
+ **/
+
+/*
+Given the heads of two singly linked-lists headA and headB, return the node at which the two lists intersect. If the two linked lists have no intersection at all, return null.
+
+For example, the following two linked lists begin to intersect at node c1:
+* */
+
+public class IntersectionTwoLinkedLists_160 {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+
+     return new ListNode(1);
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
